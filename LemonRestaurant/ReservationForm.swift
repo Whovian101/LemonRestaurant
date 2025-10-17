5b9ef077655a03a0373f0c96ac46c78f7028a349
Linda Luna <lindathihuynh@gmail.com>
2025-10-17 16:40:07 -0700

    Add ReservationForm view with @state variables and Stepper

---
+++ LemonRestaurant/ReservationForm.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationForm.swift
+//  LemonRestaurant
+//
+//  Created by Linda Luna on 10/17/25.
+//
+
+import SwiftUI
+
+struct ReservationForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReservationForm()
+}
