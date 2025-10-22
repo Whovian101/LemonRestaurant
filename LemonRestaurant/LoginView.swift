03a8c1c0ea03e54db8adc6541f8932fb5d8318cf
Linda Luna <lindathihuynh@gmail.com>
2025-10-21 17:19:34 -0700

    Changes to reservationForm to include warning messages and button disabled without user confirmation

---
+++ LemonRestaurant/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  LemonRestaurant
+//
+//  Created by Linda Luna on 10/18/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
